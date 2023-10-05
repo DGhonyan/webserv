534773cbba5ccdcab15e34d569ee155e81e2deeb
dghonyan <you@example.com>
2023-10-05 11:43:53 +0400

    working version

---
+++ src/main/webserv.cpp
@@ -0,0 +1,22 @@
+#include "TCPserver.hpp"
+
+int main(int argc, char **argv)
+{
+	Config conf(argc > 1 ? argv[1] : DEFAULT_FILE_PATH);
+
+	try
+	{
+		conf.parse();
+		// conf.print();
+
+		TCPserver socket(conf);
+
+		socket.server_loop();
+
+		return 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
